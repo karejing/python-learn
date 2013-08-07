051f8d85a2649d23f561b3901730d2fa2e95c198
karejing <guyajing@gmail.com>
2013-08-07 12:10:44 +0800

    open.py
    知识点:
    •  open的参数:r表示读,w写数据,在写之前先清空文件内容,a打开并附加内容.
    •  打开文件之后记得关闭

---
+++ open.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+spath="D:/download/baa.txt"
+f=open(spath,"w") # Opens file for writing.Creates this file doesn't exist.
+f.write("First line 1.\n")
+f.writelines("First line 2.")
+
+f.close()
+
+f=open(spath,"r") # Opens file for reading
+
+for line in f:
+    print("每一行的数据是:%s"%line)
+
+f.close()
+
+'''
+知识点:
+•  open的参数:r表示读,w写数据,在写之前先清空文件内容,a打开并附加内容.
+•	打开文件之后记得关闭
+'''
