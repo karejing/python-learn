15ef58c2e5721e29002f16092dd3c13969db5d8c
karejing <guyajing@gmail.com>
2013-08-07 12:11:32 +0800

    Create add.py
    知识点:
    
        * 用int和str函数将字符串和数字进行转换
        * 打印以#开头,而不是习惯的//
        * 打印多个参数的方式

---
+++ add.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+#a=2
+#b="test"
+#c=a+b
+
+
+a=2
+b="test"
+c=str(a)+b
+d="1111"
+e=a+int(d)
+#How to print multiply values
+print ("c is %s,e is %i" % (c,e))
+
+
+'''
+知识点:
+
+    * 用int和str函数将字符串和数字进行转换
+    * 打印以#开头,而不是习惯的//
+    * 打印多个参数的方式
+'''
