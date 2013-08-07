66a9bef87c75df22aa8738f73f257a0c249a1295
karejing <guyajing@gmail.com>
2013-08-07 12:44:41 +0800

    range.py
    知识点:
    •  Python 不用{}来控制程序结构,他强迫你用缩进来写程序,使代码清晰.
    •  定义函数方便简单
    •  方便好用的range函数

---
+++ range.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*- 
+
+# The range() function
+a =range (1,10)
+for i in a:
+    print (i)
+    
+a = range(-2,-11,-3) # The 3rd parameter stands for step
+for i in a:
+    print (i)
+
+'''
+
+知识点:
+•  Python 不用{}来控制程序结构,他强迫你用缩进来写程序,使代码清晰.
+•	定义函数方便简单
+•	方便好用的range函数
+'''
