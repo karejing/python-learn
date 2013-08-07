7bffb40faa9459d1533e038bcbeb1cc117108795
karejing <guyajing@gmail.com>
2013-08-07 12:29:14 +0800

    Create for.py
    知识点:
    * 条件和循环语句
    * 如何得到控制台输入

---
+++ for.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python
+#条件和循环语句
+
+x=int(input("Please enter an integer:"))
+if x<0:
+    x=0
+    print ("Negative changed to zero")
+
+elif x==0:
+    print ("Zero")
+
+else:
+    print ("More")
+
+
+# Loops List
+a = ['cat', 'window', 'defenestrate']
+for x in a:
+    print (x, len(x))
+
+'''
+知识点:
+
+    * 条件和循环语句
+    * 如何得到控制台输入
+'''
