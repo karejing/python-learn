2cb84ba4be139f7e1b3b326028eb91d0313f41d1
karejing <guyajing@gmail.com>
2013-08-07 12:47:42 +0800

    word-2.py
     知识点:
    •  类似Java,在python3里所有字符串都是unicode,所以长度一致.

---
+++ word-2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*- 
+
+s=input("输入你的中文名,按回车继续");
+print ("你的名字是  : " +s)
+
+l=len(s)
+print ("你中文名字的长度是:"+str(l))
+
+'''
+  知识点:
+•  类似Java,在python3里所有字符串都是unicode,所以长度一致.
+'''
